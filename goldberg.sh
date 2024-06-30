1b4c448f94e7332ee2bf3864895f026cb7405298
daci12345 <100242590+daci12345@users.noreply.github.com>
2024-06-30 10:54:38 +0200

    Update goldberg.sh

--- goldberg.sh
+++ goldberg.sh
@@ -9,22 +9,6 @@
 #config
 EMU_DIR="/path/to/emu"
 
-#funcitons
-dlc() {
-	#get games dlc list
-	DLC_ID="$(curl -s https://store.steampowered.com/api/appdetails?appids="$APPID" | jq .[].data.dlc | sed 's|[^0-9]||g' | xargs)"
-
-	#list all dlcs with names and put them into dlc.txt
-	mkdir -p steam_settings
-	echo "Getting DLC.txt for $APPID"
-	for i in $DLC_ID
-	do
-		echo "getting info for $i" && echo "$i=$(curl -s https://store.steampowered.com/api/appdetails?appids="$i" | jq -r .[].data.name)" >> "steam_settings/DLC.txt"
-	done
-
-	echo "Getting DLC.txt for $APPID completed"   
-}
-
 username() {
 	mkdir -p steam_settings
 	echo "jc141" > steam_settings/force_account_name.txt
@@ -34,18 +18,18 @@ native() {
 	mv libsteam_api.so libsteam_api.so.orig
 	if file libsteam_api.so.orig | grep -q "32-bit";
 	then
-		cp "$EMU_DIR/linux/x86/libsteam_api.so" libsteam_api.so
+		cp "$EMU_DIR/linux/x32/libsteam_api.so" libsteam_api.so
 	else
-		cp "$EMU_DIR/linux/x86_64/libsteam_api.so" libsteam_api.so
+		cp "$EMU_DIR/linux/x64/libsteam_api.so" libsteam_api.so
 	fi
 }
 
 windows() {
 	if [ -e steam_api64.dll ];
 	then
-		mv steam_api64.dll steam_api64.dll.orig && cp "$EMU_DIR/experimental/steam_api64.dll" steam_api64.dll
+		mv steam_api64.dll steam_api64.dll.orig && cp "$EMU_DIR/experimental/x64/steam_api64.dll" steam_api64.dll
 	else
-		mv steam_api.dll steam_api.dll.orig && cp "$EMU_DIR/experimental/steam_api.dll" steam_api.dll
+		mv steam_api.dll steam_api.dll.orig && cp "$EMU_DIR/experimental/x32/steam_api.dll" steam_api.dll
 	fi
 }
 
@@ -59,12 +43,12 @@ interfaces() {
 }
 
 genconf() {
-	python "$EMU_DIR/scripts/generate_emu_config.py" $APPID && cp -r "$APPID"_output/steam_settings "$PWD" && rm -rf "$PWD/login_temp" && rm -rf "$PWD/backup" && rm -rf "$APPID"_output
+ "$EMU_DIR/scripts/generate_emu_config/generate_emu_config" $APPID && cp -r output/"$APPID"/steam_settings "$PWD" && cp -r output/"$APPID"/info "$PWD" && rm -rf output
 }
 
 if [ -e libsteam_api.so ];
 then
-	interfaces & sleep 1 && native && username && dlc && genconf
+	interfaces & sleep 1 && native && username && genconf
 else
-	interfaces & sleep 1 && windows && username && dlc && genconf
+	interfaces & sleep 1 && windows && username && genconf
 fi
